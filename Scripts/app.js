ae33015699abdd48c7cb11240e870f71e80a9170
MattTheBrains <matthew.ware@dcmail.ca>
2022-02-09 14:03:06 -0500

    Contact form ready, timer needs fixed

---
+++ Scripts/app.js
@@ -0,0 +1,7 @@
+function postForm(){
+    console.log("Name: " + document.forms["aForm"]["name"].value);
+    console.log("Number: " + document.forms["aForm"]["number"].value);
+    console.log("Email: " + document.forms["aForm"]["email"].value);
+    console.log("Comments: " + document.forms["aForm"]["comments"].value);
+    setTimeout(document.getElementById("aForm").submit(), 3000);
+}
