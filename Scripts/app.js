62f36c17807b476d446ce0c75db84f9dfc32cf17
Irina <nazarova1800546@mail.ru>
2022-02-11 01:01:51 -0500

    *

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,16 +1,16 @@
 /*
 WEBD6201 - Lab 1
 Matthew Ware - 100472787
-Irina Nazarova - 
+Irina Nazarova - 10078306
 Date Completed - November 11, 2022
 */
 // Variables used to contain strings of text
 var welcomeMsg = "You have arrived at the WEBD 6201 Lab 1 Website for Matthew Ware and Irina Nazarova, Weclome to our website.  We have included links to our projects, services, about us, and a way to contact us.  Enjoy!";
 var aboutMatt = "I am a Durham College student in the Computer Programmer Analyst three year diploma.  I have a BA in Philosophy, a BSc in Psychology with a minor in Physics, a BA in Adult Education and Digital Technology, and hope to achieve another BSc in Computer Science.";
-var aboutIrina = "I am a Durham College student in ....";
-var projectsMatt = "The top three projects by Matt include a website to sort notes, a site to take notes for my next book, and my database program.";
-var projectsIrina = "The top three projects by Irina include...";
-var servicesMsg = "The top three services offered by Matt include programming, web development, and databases.  The three top services offered by Irina include ...";
+var aboutIrina = "I am a Durham College student in Computer Programming two year diploma";
+var projectsMatt = "The top three projects by Matt include a <b>website to sort notes</b>, a <b>site to take notes for my next book</b>, and my <b>database program</b>.";
+var projectsIrina = "The top three projects by Irina include <b>Tic Tac Toe game</b>, <b>message encryption ui program</b>, and <b>small database python project</b>";
+var servicesMsg = "The top 3 services offered by: <ul><li><b>Matt</b> <ul> <li> programming</li> <li>web development</li><li>databases</li></ul><li><b>Irina</b><ul><li>UI programming</li><li>Tutoring</li><li>Software development</li></li></ul>";
 var addedLink = "<a style='padding:20px' class='humanresources' href='humanResources.html'>Human Resources</a>";
 
 // Submits the form after the delay function is called
